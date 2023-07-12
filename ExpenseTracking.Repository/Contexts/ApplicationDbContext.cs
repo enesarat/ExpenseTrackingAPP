c6896dbdcfa53e9a7286aae8b8377f68cf914057
Enes Arat <enes_arat@outlook.com>
2023-07-12 13:48:15 +0300

    Contains: Some Ef Core packages have been installed and the app db context class has been created.

---
+++ ExpenseTracking.Repository/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using ExpenseTracking.Core.Models.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracking.Repository.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<PaymentType> PaymentTypes { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            foreach (var relationship in builder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            base.OnModelCreating(builder);
+
+        }
+    }
+}
