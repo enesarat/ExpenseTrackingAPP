a4e9a88144fc58fa4f1f55a36a6e0c4815271188
Enes Arat <enes_arat@outlook.com>
2023-07-12 13:29:19 +0300

    Contains: An interface was created to be used in the UnitOfWork design pattern.

---
+++ ExpenseTracking.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using ExpenseTracking.Core.Models.Concrete;
+using ExpenseTracking.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracking.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync();
+        void Commit();
+    }
+}
