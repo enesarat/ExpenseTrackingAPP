6879f9abe2b974f0b79ce4f53f6c340d08726280
Enes Arat <enes_arat@outlook.com>
2023-07-12 18:52:11 +0300

    Contains: CustomResponse, NoContentResponse and CustomBaseController created.

---
+++ ExpenseTracking.Core/DTOs/Concrete/Response/NoContentResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracking.Core.DTOs.Concrete.Response
+{
+    public class NoContentResponse
+    {
+    }
+}
